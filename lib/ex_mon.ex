edb26c4652affbe7853c2cdcb5257fdc492cf558
Sam Marxz <sammarxzz@gmail.com>
2020-11-12 14:59:17 -0300

    Add Player Module. Add Player Struct.

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,17 +1,4 @@
 defmodule ExMon do
-  @moduledoc """
-  Documentation for `ExMon`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExMon.hello()
-      :world
-
-  """
   def hello(name) do
     "Hello #{name}"
   end
