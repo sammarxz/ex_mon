49ed84e9bbbb7cb37992c6300d5f11073c8c56cb
Sam Marxz <sammarxzz@gmail.com>
2020-11-12 15:16:38 -0300

    Player Struct Refatoring.

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,6 @@
 defmodule ExMon do
-  def hello(name) do
-    "Hello #{name}"
-  end
+  alias ExMon.Player  
+  def create_player(name, move_avg, move_rnd, move_heal) do
+    Player.build(name, move_avg, move_rnd, move_heal)
+   end
 end
